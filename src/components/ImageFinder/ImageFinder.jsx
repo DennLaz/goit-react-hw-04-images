99b517a93412aed3022ffb30976ed592f269b0e6
Denis Lazarenko <dentus25@gmail.com>
2022-07-05 22:28:44 +0300

    add_files

--- src/components/ImageFinder/ImageFinder.jsx
+++ src/components/ImageFinder/ImageFinder.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import Searchbar from './Searchbar';
 import { getImages } from '../../shared/services/getApi';
@@ -8,65 +9,74 @@ import Modal from 'shared/Modal';
 import Loader from './Loader';
 import styles from './imageFinder.module.css';
 
-class ImageFinder extends Component {
-  state = {
+export default function ImageFinder() {
+  const [state, setState] = useState({
     items: [],
+    loader: false,
+    error: null,
+  });
+
+  const [pageQuery, setPageQuery] = useState({
     query: '',
     page: 1,
     totalPage: 0,
+  });
+
+  const [modal, setModal] = useState({
     modalImg: {
       src: '',
       alt: '',
     },
     modalOpen: false,
-    loader: false,
-    error: null,
-  };
+  });
 
-  componentDidUpdate(prevProps, prevState) {
-    const { page: prevPage, query: prevQuery } = prevState;
-    const { page: nextPage, query: nextQuery } = this.state;
-    if (prevPage !== nextPage || prevQuery !== nextQuery) {
-      this.getImgItems();
+  const { items, loader, error } = state;
+  const { query, page, totalPage } = pageQuery;
+  const { modalOpen, modalImg } = modal;
+
+  useEffect(() => {
+    async function getImgItems() {
+      setState(prevState => ({ ...prevState, error: false, loader: true }));
+      try {
+        const data = await getImages(query, page);
+        const totalPage = Math.ceil(data.totalHits / 12);
+        setState(
+          prevState => {
+            return {
+              ...prevState,
+              items: [...prevState.items, ...data.hits],
+            };
+          },
+          setPageQuery(prevPageQuery => {
+            return { ...prevPageQuery, totalPage };
+          })
+        );
+      } catch (error) {
+        setState(prevState => ({ ...prevState, error}));
+      }
     }
-  }
+    getImgItems();
+  }, [query, page]);
 
-  async getImgItems() {
-    const { query, page } = this.state;
-    this.setState({ loader: true });
-    try {
-      const data = await getImages(query, page);
-      const totalPage = Math.ceil(data.totalHits / 12);
-      this.setState(prevState => {
-        const { items } = prevState;
-
-        return { items: [...items, ...data.hits], totalPage, loader: false };
-      });
-    } catch (error) {
-      this.setState({ error, loader: false });
-    }
-  }
-
-  saveQuery = value => {
-    const { query } = this.state;
+  function saveQuery(value) {
     if (value !== query) {
-      this.setState({ query: value, page: 1, totalPage: 0, items: [] });
+      setPageQuery({ query: value, page: 1, totalPage: 0 });
+      setState(prevState => ({ ...prevState, items: [] }));
     }
-  };
+  }
 
-  loadMoreClick = () => {
-    this.setState(prevState => {
+  function loadMoreClick() {
+    setPageQuery(prevState => {
       const { page: prevPage } = prevState;
-      return { page: prevPage + 1 };
+      return { ...prevState, page: prevPage + 1 };
     });
-  };
+  }
 
-  openModal = indx => {
-    const { items, modalImg } = this.state;
+  function openModal(indx) {
     const { src: currentSrc } = modalImg;
     const { largeImageURL: src, tags: alt } = items[indx];
     if (currentSrc !== src) {
-      this.setState({
+      setModal({
         modalImg: {
           alt,
           src,
@@ -74,56 +84,162 @@ class ImageFinder extends Component {
         modalOpen: true,
       });
     }
-  };
+  }
 
-  closeModal = () => {
-    this.setState({
+  function closeModal() {
+    setModal({
       modalImg: {
         src: '',
         alt: '',
       },
       modalOpen: false,
     });
-  };
-
-  render() {
-    const { saveQuery, loadMoreClick, openModal, closeModal } = this;
-    const {
-      items,
-      modalImg,
-      modalOpen,
-      totalPage,
-      page,
-      loader,
-      query,
-      error,
-    } = this.state;
-
-    const notFound = !totalPage && query && !loader && !error;
-    const noWrapper = error || loader || notFound;
-    const noGallary = !notFound && query && !error;
-    const noButton = totalPage - page > 0;
-
-    return (
-      <div className={styles.imageFinder}>
-        <Searchbar onSubmit={saveQuery} />
-        {noGallary && <ImageGallery items={items} onClick={openModal} />}
-        {noButton && !loader && <Button onClick={loadMoreClick} />}
-        {modalOpen && (
-          <Modal onClose={closeModal}>
-            <img src={modalImg.src} alt={modalImg.alt} />
-          </Modal>
-        )}
-        {noWrapper && (
-          <div className={styles.wrapper}>
-            {loader && <Loader />}
-            {error && <p className={styles.error}>{error?.message}</p>}
-            {notFound && <p className={styles.notFind}>{`On request "${query}" nothing found`}</p>}
-          </div>
-        )}
-      </div>
-    );
   }
+
+  const notFound = !totalPage && query && !loader && !error;
+  const noWrapper = error || loader || notFound;
+  const noGallary = !notFound && query && !error;
+  const noButton = totalPage - page > 0;
+
+  return (
+    <div className={styles.imageFinder}>
+      <Searchbar onSubmit={saveQuery} />
+      {noGallary && <ImageGallery items={items} onClick={openModal} />}
+      {noButton && !loader && <Button onClick={loadMoreClick} />}
+      {modalOpen && (
+        <Modal onClose={closeModal}>
+          <img src={modalImg.src} alt={modalImg.alt} />
+        </Modal>
+      )}
+      {noWrapper && (
+        <div className={styles.wrapper}>
+          {loader && <Loader />}
+          {error && <p className={styles.error}>{error?.message}</p>}
+          {notFound && (
+            <p
+              className={styles.notFind}
+            >{`On request "${query}" nothing found`}</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default ImageFinder;
+// class OldImageFinder extends Component {
+//   // state = {
+//   //   items: [],
+//   //   query: '',
+//   //   page: 1,
+//   //   totalPage: 0,
+//   //   modalImg: {
+//   //     src: '',
+//   //     alt: '',
+//   //   },
+//   //   modalOpen: false,
+//   //   loader: false,
+//   //   error: null,
+//   // };
+
+//   // componentDidUpdate(prevProps, prevState) {
+//   //   const { page: prevPage, query: prevQuery } = prevState;
+//   //   const { page: nextPage, query: nextQuery } = this.state;
+//   //   if (prevPage !== nextPage || prevQuery !== nextQuery) {
+//   //     this.getImgItems();
+//   //   }
+//   // }
+
+//   // async getImgItems() {
+//   //   const { query, page } = this.state;
+//   //   this.setState({ loader: true });
+//   //   try {
+//   //     const data = await getImages(query, page);
+//   //     const totalPage = Math.ceil(data.totalHits / 12);
+//   //     this.setState(prevState => {
+//   //       const { items } = prevState;
+
+//   //       return { items: [...items, ...data.hits], totalPage, loader: false };
+//   //     });
+//   //   } catch (error) {
+//   //     this.setState({ error, loader: false });
+//   //   }
+//   // }
+
+//   // saveQuery = value => {
+//   //   const { query } = this.state;
+//   //   if (value !== query) {
+//   //     this.setState({ query: value, page: 1, totalPage: 0, items: [] });
+//   //   }
+//   // };
+
+//   // loadMoreClick = () => {
+//   //   this.setState(prevState => {
+//   //     const { page: prevPage } = prevState;
+//   //     return { page: prevPage + 1 };
+//   //   });
+//   // };
+
+//   // openModal = indx => {
+//   //   const { items, modalImg } = this.state;
+//   //   const { src: currentSrc } = modalImg;
+//   //   const { largeImageURL: src, tags: alt } = items[indx];
+//   //   if (currentSrc !== src) {
+//   //     this.setState({
+//   //       modalImg: {
+//   //         alt,
+//   //         src,
+//   //       },
+//   //       modalOpen: true,
+//   //     });
+//   //   }
+//   // };
+
+//   // closeModal = () => {
+//   //   this.setState({
+//   //     modalImg: {
+//   //       src: '',
+//   //       alt: '',
+//   //     },
+//   //     modalOpen: false,
+//   //   });
+//   // };
+
+//   // render() {
+//   // const { saveQuery, loadMoreClick, openModal, closeModal } = this;
+//   // const {
+//   //   items,
+//   //   modalImg,
+//   //   modalOpen,
+//   //   totalPage,
+//   //   page,
+//   //   loader,
+//   //   query,
+//   //   error,
+//   // } = this.state;
+
+//   // const notFound = !totalPage && query && !loader && !error;
+//   // const noWrapper = error || loader || notFound;
+//   // const noGallary = !notFound && query && !error;
+//   // const noButton = totalPage - page > 0;
+
+//   // return (
+//   //   <div className={styles.imageFinder}>
+//   //     <Searchbar onSubmit={saveQuery} />
+//   //     {noGallary && <ImageGallery items={items} onClick={openModal} />}
+//   //     {noButton && !loader && <Button onClick={loadMoreClick} />}
+//   //     {modalOpen && (
+//   //       <Modal onClose={closeModal}>
+//   //         <img src={modalImg.src} alt={modalImg.alt} />
+//   //       </Modal>
+//   //     )}
+//   //     {noWrapper && (
+//   //       <div className={styles.wrapper}>
+//   //         {loader && <Loader />}
+//   //         {error && <p className={styles.error}>{error?.message}</p>}
+//   //         {notFound && <p className={styles.notFind}>{`On request "${query}" nothing found`}</p>}
+//   //       </div>
+//   //     )}
+//   //   </div>
+//   // );
+//   // }
+// }
--- src/components/ImageFinder/Searchbar/Searchbar.jsx
+++ src/components/ImageFinder/Searchbar/Searchbar.jsx
@@ -1,60 +1,97 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
+
 import { ImSearch } from 'react-icons/im';
 
 import PropTypes from 'prop-types';
 
 import styles from './searchbar.module.css';
 
-class Searchbar extends Component {
+export default function Searchbar({ onSubmit }) {
+  const [value, setValue] = useState('');
 
-  state = {
-    value: '',
-  };
+  function handleValueChange({ target }) {
+    setValue(target.value);
+  }
 
-  handleValueChange = ({ target: { value} }) => {
-    this.setState({ value })
-  };
-
-  heandleSubmit = (e) => {
+  function heandleSubmit(e) {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    const { value } = this.state;
-    onSubmit(value)
+    onSubmit(value);
   }
 
-  render() {
-    const { handleValueChange, heandleSubmit } = this;
-    const { value } = this.state;
-    return (
-      <header className={styles.searchbar}>
-        <form onSubmit={heandleSubmit} className={styles.searchForm}>
-          <button type="submit" className={styles.searchFormButton}>
-            <ImSearch />
-            <span className={styles.searchFormButtonLabel}>Search</span>
-          </button>
-          
-          <input
-            onChange={handleValueChange}
-            value={value}
-            className={styles.searchFormInput}
-            type="text"
-            placeholder="Search images and photos"
-            required
-            autoComplete="off"
-            autoFocus
-          />
-        </form>
-      </header>
-    );
-  }
+  return (
+    <header className={styles.searchbar}>
+      <form onSubmit={heandleSubmit} className={styles.searchForm}>
+        <button type="submit" className={styles.searchFormButton}>
+          <ImSearch />
+          <span className={styles.searchFormButtonLabel}>Search</span>
+        </button>
+
+        <input
+          onChange={handleValueChange}
+          value={value}
+          className={styles.searchFormInput}
+          type="text"
+          placeholder="Search images and photos"
+          required
+          autoComplete="off"
+          autoFocus
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.defaltProps = {
-  onSubmit: ()=>{}
-}
+  onSubmit: () => {},
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-}
+};
 
-export default Searchbar;
+
+// class OldSearchbar extends Component {
+
+// // state = {
+// //   value: '',
+// // };
+
+// // handleValueChange = ({ target: { value} }) => {
+// //   this.setState({ value })
+// // };
+
+// // heandleSubmit = (e) => {
+// //   e.preventDefault();
+// //   const { onSubmit } = this.props;
+// //   const { value } = this.state;
+// //   onSubmit(value)
+// // }
+
+// // render() {
+// // const { handleValueChange, heandleSubmit } = this;
+// // const { value } = this.state;
+// // return (
+// //   <header className={styles.searchbar}>
+// //     <form onSubmit={heandleSubmit} className={styles.searchForm}>
+// //       <button type="submit" className={styles.searchFormButton}>
+// //         <ImSearch />
+// //         <span className={styles.searchFormButtonLabel}>Search</span>
+// //       </button>
+
+// //       <input
+// //         onChange={handleValueChange}
+// //         value={value}
+// //         className={styles.searchFormInput}
+// //         type="text"
+// //         placeholder="Search images and photos"
+// //         required
+// //         autoComplete="off"
+// //         autoFocus
+// //       />
+// //     </form>
+// //   </header>
+// // );
+// // }
+// }
+
